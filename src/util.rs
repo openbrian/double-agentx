22644e9d020a6703716b06815545babf92557232
Brian <2524550+openbrian@users.noreply.github.com>
2025-08-31 23:36:57 -0400

    Add a test for as_vec.

--- src/util.rs
+++ src/util.rs
@@ -6,3 +6,19 @@ pub fn as_vec(value: &ID) -> Vec<u32> {
         .map(|s| s.parse::<u32>().unwrap())
         .collect::<Vec<u32>>()
 }
+
+
+#[cfg(test)]
+mod tests {
+    use agentx::encodings;
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let vec = vec![1, 3, 6, 1, 2, 1, 1, 1, 0];
+        let oid = encodings::ID::try_from(vec.clone())
+            .expect("Failed to convert");
+        let result = as_vec(&oid);
+        assert_eq!(result, vec);
+    }
+}
